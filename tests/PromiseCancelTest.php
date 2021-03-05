4e9e10a13e518d40a9c4d6a7951dc93d0be9fa75
zhangdonghai <zhangdonghai@peilian.com>
2021-03-05 11:27:57 +0800

    nested promise cancel

--- tests/PromiseCancelTest.php
+++ tests/PromiseCancelTest.php
@@ -3,6 +3,9 @@
 namespace Sue\Tests\Coroutine;
 
 use Sue\Tests\Coroutine\BaseTestCase;
+
+use function React\Promise\reject;
+use function React\Promise\resolve;
 use function Sue\Coroutine\co;
 
 final class PromiseCancelTset extends BaseTestCase
@@ -26,5 +29,30 @@ final class PromiseCancelTset extends BaseTestCase
         $result = self::unwrapSettledPromise($coroutine_promise);
         $this->assertEquals(new \Sue\Coroutine\CoroutineException('Coroutine is canncelled by promise cancel'), $result);
         $this->assertEquals(true, $deferred_cancelled);
+    }
+
+    public function testAwaitPromiseCancel()
+    {
+        $promise_cancelled = false;
+        $deferred = new \React\Promise\Deferred(function () use (&$promise_cancelled) {
+            $promise_cancelled = true;
+        });
+
+        /** @var \React\Promise\CancellablePromiseInterface $coroutine_promise */
+        $coroutine_promise = co(function ($promise) {
+            yield [
+                reject(new \Exception('foo')),
+                resolve('bar'),
+                $promise
+            ];
+        }, $deferred->promise());
+
+        self::$loop->addTimer(0.5, function () use ($coroutine_promise) {
+            $coroutine_promise->cancel();
+        });
+        self::$loop->run();
+        $result = self::unwrapSettledPromise($coroutine_promise);
+        $this->assertEquals(new \Sue\Coroutine\CoroutineException('Coroutine is canncelled by promise cancel'), $result);
+        $this->assertEquals(true, $promise_cancelled);
     }
 }
